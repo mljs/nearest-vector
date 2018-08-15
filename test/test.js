0865b29ae1c26da757db8b045a6c8ef114d610d2
Daniel Kostro <kostro.d@gmail.com>
2018-08-15 17:49:31 +0200

    switch to jest for tests

--- test/test.js
+++ test/test.js
@@ -3,53 +3,60 @@
 const nearestVector = require('..');
 
 function cosineSimilarity(a, b) {
-    var ii = a.length,
-        p = 0,
-        p2 = 0,
-        q2 = 0;
-    for (var i = 0; i < ii; i++) {
-        p += a[i] * b[i];
-        p2 += a[i] * a[i];
-        q2 += b[i] * b[i];
-    }
-    return p / (Math.sqrt(p2) * Math.sqrt(q2));
+  var ii = a.length;
+
+  var p = 0;
+
+  var p2 = 0;
+
+  var q2 = 0;
+  for (var i = 0; i < ii; i++) {
+    p += a[i] * b[i];
+    p2 += a[i] * a[i];
+    q2 += b[i] * b[i];
+  }
+  return p / (Math.sqrt(p2) * Math.sqrt(q2));
 }
 
-describe('nearest-vector test', function () {
-    it('Distance tests', function () {
-        let centers = [[1, 2, 1], [-1, -1, -1]];
-        let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
-        let clusterID = [0, 0, 1, 1];
+describe('nearest-vector test', () => {
+  test('Distance tests', () => {
+    let centers = [[1, 2, 1], [-1, -1, -1]];
+    let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
+    let clusterID = [0, 0, 1, 1];
 
-        for (let i = 0; i < clusterID.length; i++) {
-            nearestVector(centers, data[i]).should.be.equal(clusterID[i]);
-        }
-    });
+    for (let i = 0; i < clusterID.length; i++) {
+      expect(nearestVector(centers, data[i])).toBe(clusterID[i]);
+    }
+  });
 
-    it('Similarity tests', function () {
-        let centers = [[1, 2, 1], [-1, -1, -1]];
-        let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
-        let clusterID = [0, 0, 1, 1];
+  test('Similarity tests', () => {
+    let centers = [[1, 2, 1], [-1, -1, -1]];
+    let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
+    let clusterID = [0, 0, 1, 1];
 
-        // cosine similarity
-        let options = {similarityFunction: cosineSimilarity};
+    // cosine similarity
+    let options = { similarityFunction: cosineSimilarity };
 
-        for (let i = 0; i < clusterID.length; i++) {
-            nearestVector(centers, data[i], options).should.be.equal(clusterID[i]);
-        }
-    });
+    for (let i = 0; i < clusterID.length; i++) {
+      expect(nearestVector(centers, data[i], options)).toBe(clusterID[i]);
+    }
+  });
 
-    it('Throws for non-function', function () {
-        nearestVector.bind(null, [], [], {distanceFunction: 1}).should.throw('A similarity or distance function it\'s required');
-    });
+  test('Throws for non-function', () => {
+    expect(
+      nearestVector.bind(null, [], [], { distanceFunction: 1 })
+    ).toThrowError("A similarity or distance function it's required");
+  });
 
-    it('Return object tests', function () {
-        let centers = [[1, 2, 1], [-1, -1, -1]];
-        let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
-        let clusterID = [0, 0, 1, 1];
+  test('Return object tests', () => {
+    let centers = [[1, 2, 1], [-1, -1, -1]];
+    let data = [[1, 1, 1], [1, 2, 1], [-1, -1, -1], [-1, -1, -1.5]];
+    let clusterID = [0, 0, 1, 1];
 
-        for (let i = 0; i < clusterID.length; i++) {
-            nearestVector(centers, data[i], {returnVector: true}).should.deepEqual(centers[clusterID[i]]);
-        }
-    });
+    for (let i = 0; i < clusterID.length; i++) {
+      expect(nearestVector(centers, data[i], { returnVector: true })).toEqual(
+        centers[clusterID[i]]
+      );
+    }
+  });
 });
